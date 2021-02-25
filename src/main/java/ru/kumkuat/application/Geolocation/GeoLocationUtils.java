7aa7b532b02faf0b3920ac3f82b498ce93d6d892
ultinigg <Lutik86>
2021-02-25 13:11:57 +0300

    Add second bot and BotController.java

--- src/main/java/ru/kumkuat/application/Geolocation/GeoLocationUtils.java
+++ src/main/java/ru/kumkuat/application/Geolocation/GeoLocationUtils.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Location;
 import ru.kumkuat.application.Service.GeolocationService;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static java.lang.Math.*;
-import static java.lang.Math.pow;
 
 @Service
 public class GeoLocationUtils {
@@ -22,45 +20,45 @@ public class GeoLocationUtils {
         this.geolocationService = geolocationService;
     }
 
-    public  Double distanceToCurrentLocation(Double userLati, Double userLong, Double aimLati, Double aimLong) {
-        Double EARTH_RADIUS = 6372795.0;
+    public  double distanceToCurrentLocation(Double userLati, Double userLong, Double aimLati, Double aimLong) {
+        double EARTH_RADIUS = 6372795.0;
 
         //переводим координаты в радианы
         double latUser = userLati * PI / 180;
-        Double longUser = userLong * PI / 180;
+        double longUser = userLong * PI / 180;
         double latAim = aimLati * PI / 180;
-        Double longAim = aimLong * PI / 180;
+        double longAim = aimLong * PI / 180;
 
         // косинусы и синусы широт и разницы долгот
-        Double cl1 = cos(latUser);
-        Double sl1 = sin(latUser);
-        Double cl2 = cos(latAim);
-        Double sl2 = sin(latAim);
+        double cl1 = cos(latUser);
+        double sl1 = sin(latUser);
+        double cl2 = cos(latAim);
+        double sl2 = sin(latAim);
         double deltaLong = longAim - longUser;
-        Double cosDelta = cos(deltaLong);
-        Double sinDelta = sin(deltaLong);
+        double cosDelta = cos(deltaLong);
+        double sinDelta = sin(deltaLong);
 
         //вычисление длины большого круга
         double y = Math.sqrt(pow(cl2 * sinDelta, 2) + pow(cl1 * sl2 - sl1 * cl2 * cosDelta, 2));
         double x = sl1 * sl2 + cl1 * cl2 * cosDelta;
 
-        Double ad = Math.atan2(y, x);
+        double ad = Math.atan2(y, x);
         return ad * EARTH_RADIUS;
     }
 
 
     public  Geolocation nearestLocation(List<Geolocation> geolocationList, Location userLocation) {
-        Double userLong = userLocation.getLongitude();
-        Double userLat = userLocation.getLatitude();
-        Double min = 99999999.0;
+        double userLong = userLocation.getLongitude();
+        double userLat = userLocation.getLatitude();
+        double min = 99999999.0;
         String fullName = "";
-        Double resultLatitude = 0.0;
-        Double resulLongitude = 0.0;
+        double resultLatitude = 0.0;
+        double resulLongitude = 0.0;
         for (Geolocation entry : geolocationList) {
 
-            Double longList = entry.getLongitude();
-            Double latList = entry.getLatitude();
-            Double temp = distanceToCurrentLocation(userLat, userLong, latList, longList);
+            double longList = entry.getLongitude();
+            double latList = entry.getLatitude();
+            double temp = distanceToCurrentLocation(userLat, userLong, latList, longList);
             if (temp < min) {
                 min = temp;
                 fullName = entry.getFullName();
@@ -77,7 +75,7 @@ public class GeoLocationUtils {
 
 
     public Map<String, Object> foundNearestLocationService(Location userLocation) {
-        Double distance;
+        double distance;
         Map<String, Object> resultList = new HashMap<>();
         List<Geolocation> geolocationList1 = geolocationService.getAll();
         Geolocation nearestGeolocation = nearestLocation(geolocationList1, userLocation);
