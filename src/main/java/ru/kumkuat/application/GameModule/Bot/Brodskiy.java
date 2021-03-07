dc86d564eaefafb820b111a0ab113b467edd7ae8
ultinigg <Lutik86>
2021-03-07 23:51:21 +0300

    Add UpdateController (empty)

---
+++ src/main/java/ru/kumkuat/application/GameModule/Bot/Brodskiy.java
@@ -0,0 +1,17 @@
+package ru.kumkuat.application.GameModule.Bot;
+
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+public class Brodskiy extends BotCommand {
+    public Brodskiy(String commandIdentifier, String description) {
+        super(commandIdentifier, description);
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+    }
+}
