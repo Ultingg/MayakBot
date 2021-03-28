a985125730df228672b61e14be1c7a90ede9c452
ultinigg <Lutik86>
2021-03-28 23:43:50 +0300

    Refactor Exceptions

---
+++ src/main/java/ru/kumkuat/application/GameModule/Exceptions/TriggerException.java
@@ -0,0 +1,10 @@
+package ru.kumkuat.application.GameModule.Exceptions;
+
+public class TriggerException extends Exception {
+    public TriggerException() {
+    }
+
+    public TriggerException(String message) {
+        super(message);
+    }
+}
