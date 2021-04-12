673ac726e88a8bf5fd59b82c0d263a946462fdcb
Max Novikov <maximnn1720@gmail.com>
2021-04-12 22:03:24 +0300

    Bots reply creates in new tread

--- src/main/java/ru/kumkuat/application/GameModule/Controller/UpdateController.java
+++ src/main/java/ru/kumkuat/application/GameModule/Controller/UpdateController.java
@@ -12,9 +12,34 @@ import ru.kumkuat.application.GameModule.Bot.Brodskiy;
 import ru.kumkuat.application.GameModule.Bot.MarshakBot;
 import ru.kumkuat.application.GameModule.Service.ResponseService;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 @Slf4j
 @RestController
 public class UpdateController {
+
+    class CallBotResponse implements Callable<Boolean> {
+        Update update;
+
+        CallBotResponse(Update update) {
+            this.update = update;
+        }
+
+        @Override
+        public Boolean call() throws Exception {
+            System.out.println("Привет из побочного потока!");
+            Message incomingMessage = update.getMessage();
+            if (commandChecker(incomingMessage)) {
+                log.debug("Received throw to Marshak.");
+                responseService.messageReceiver(incomingMessage, navigationCommandCheck(incomingMessage));
+            } else {
+                log.debug("Received message.");
+                responseService.messageReceiver(incomingMessage, false);
+            }
+            return true;
+        }
+    }
 
     private final MarshakBot marshakBot;
     private final Brodskiy brodskiy; // бот слушатель
@@ -24,20 +49,14 @@ public class UpdateController {
         this.marshakBot = marshakBot;
         this.brodskiy = brodskiy;
         this.responseService = responseService;
-
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
         if (update.getMessage() != null) {
-            Message incomingMessage = update.getMessage();
-            if (commandChecker(incomingMessage)) {
-                log.debug("Received throw to Marshak.");
-                responseService.messageReceiver(incomingMessage, navigationCommandCheck(incomingMessage));
-            } else {
-                log.debug("Received message.");
-                responseService.messageReceiver(incomingMessage, false);
-            }
+            FutureTask<Boolean> task = new FutureTask<>(new CallBotResponse(update));
+            Thread myThready = new Thread(task);
+            myThready.start();
         }
         //Тут вся механия распределения сообщений
         return brodskiy.onWebhookUpdateReceived(update); // слушатель возвращает на сервер Телеграмма HTTP 200(OK) с пустым сообщением
