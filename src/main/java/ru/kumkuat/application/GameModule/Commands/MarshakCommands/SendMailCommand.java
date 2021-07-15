994418c0e599b942e5125f2f1efe163392d75261
Max Novikov <maximnn1720@gmail.com>
2021-07-15 20:51:54 +0300

    sent email

--- src/main/java/ru/kumkuat/application/GameModule/Commands/MarshakCommands/SendMailCommand.java
+++ src/main/java/ru/kumkuat/application/GameModule/Commands/MarshakCommands/SendMailCommand.java
@@ -2,34 +2,26 @@ package ru.kumkuat.application.GameModule.Commands.MarshakCommands;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
-import org.telegram.telegrambots.meta.api.methods.groupadministration.ExportChatInviteLink;
-import org.telegram.telegrambots.meta.api.methods.groupadministration.UnbanChatMember;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.kumkuat.application.GameModule.Bot.MarshakBot;
-import ru.kumkuat.application.GameModule.Service.TelegramChatService;
-import ru.kumkuat.application.GameModule.Service.UserService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import ru.kumkuat.application.GameModule.Controller.MailController;
 
 @Slf4j
 @Service
-@Controller
+
 public class SendMailCommand extends BotCommand {
 
     @Autowired
-    private JavaMailSender emailSender;
-
+    private MailController mailController;
+    @Autowired
+    private TemplateEngine templateEngine;
 
     public SendMailCommand() {
         super("/send_mail", "Направить пользователю письмо на почту");
@@ -37,20 +29,10 @@ public class SendMailCommand extends BotCommand {
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
-        sendSimpleEmail();
-    }
-
-    @ResponseBody
-    @RequestMapping("/sendEmail")
-    public String sendSimpleEmail() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("solnce-3@yandex.ru");
-        message.setTo("maximnn1720@gmail.com");
-        message.setSubject("Hello");
-        message.setText("Hello, Im testing Simple Email");
-        this.emailSender.send(message);
-
-        return "Email Sent!";
+        Context context = new Context();
+        context.setVariable("user", "value");
+        var text = templateEngine.process("Emails/Welcome.html", context);
+        mailController.sendSimpleEmail("maximnn1720@gmail.com", "title", text);
     }
 
     void execute(AbsSender sender, SendMessage message, User user) {
--- src/main/java/ru/kumkuat/application/MailConfiguration.java
+++ src/main/java/ru/kumkuat/application/MailConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.thymeleaf.TemplateEngine;
 
 import java.util.Properties;
 
