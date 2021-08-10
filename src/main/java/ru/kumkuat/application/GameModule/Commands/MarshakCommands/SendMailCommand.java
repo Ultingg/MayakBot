04b906d92b106b5880fde6a6d7e10094994b3a4a
ultinigg <ultinigg@gmail.com>
2021-08-11 00:06:05 +0300

    Return mail services

--- src/main/java/ru/kumkuat/application/GameModule/Commands/MarshakCommands/SendMailCommand.java
+++ src/main/java/ru/kumkuat/application/GameModule/Commands/MarshakCommands/SendMailCommand.java
@@ -10,6 +10,7 @@ import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import ru.kumkuat.application.GameModule.Controller.MailController;
 import ru.kumkuat.application.GameModule.Service.BGUserService;
 
@@ -31,18 +32,18 @@ public class SendMailCommand extends BotCommand {
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
-//        if (arguments != null && arguments.length > 0 && arguments[0].equals("all")) {
-//            for (var bgUser :
-//                    bgUserService.getAllNotNotifedUsers()) {
-//                Context context = new Context();
-//                context.setVariable("user", bgUser.getFirstName() + " " + bgUser.getSecondName());
-//                context.setVariable("starttime", bgUser.getStartTime());
-//                var text = templateEngine.process("Emails/Welcome.html", context);
-//                mailController.sendSimpleEmail(bgUser.getEmail(), "ProSpectSpb", text);
-//                bgUser.setIsNotified(true);
-//                bgUserService.setBGUserToDB(bgUser);
-//            }
-//        }
+        if (arguments != null && arguments.length > 0 && arguments[0].equals("all")) {
+            for (var bgUser :
+                    bgUserService.getAllNotNotifedUsers()) {
+                Context context = new Context();
+                context.setVariable("user", bgUser.getFirstName() + " " + bgUser.getSecondName());
+                context.setVariable("starttime", bgUser.getStartTime());
+                var text = templateEngine.process("Emails/Welcome.html", context);
+                mailController.sendSimpleEmail(bgUser.getEmail(), "ProSpectSpb", text);
+                bgUser.setIsNotified(true);
+                bgUserService.setBGUserToDB(bgUser);
+            }
+        }
     }
 
     void execute(AbsSender sender, SendMessage message, User user) {
